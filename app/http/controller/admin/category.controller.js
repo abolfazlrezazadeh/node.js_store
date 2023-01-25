12f431e8ae555a4981f6389bbd36c307a6da5f49
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-01-25 12:18:21 +0330

    completing category

--- app/http/controller/admin/category.controller.js
+++ app/http/controller/admin/category.controller.js
@@ -4,37 +4,113 @@ const controller = require("../controller");
 const { addCategorySchema } = require("../../validator/admin/category.schema");
 
 class categoryController extends controller {
-
   async addCategory(req, res, next) {
-      try {
-        console.log("1");
-        await addCategorySchema.validateAsync(req.body);
-    const { title, parent } = req.body;
-    const category = await categoryModel.create({ title, parent });
-    if (!category) {
-      throw createErrors.InternalServerError(
-        "creating category is not successfully"
-      );
-    }
-    return res.status(201).send({
-      data: {
-        statusCode: 201,
-        message: "the category was created successfully",
-      },
-    });
+    try {
+      await addCategorySchema.validateAsync(req.body);
+      const { title, parent } = req.body;
+      const category = await categoryModel.create({ title, parent });
+      if (!category) {
+        throw createErrors.InternalServerError(
+          "creating category is not successfully"
+        );
+      }
+      return res.status(201).send({
+        data: {
+          statusCode: 201,
+          message: "the category was created successfully",
+        },
+      });
     } catch (error) {
       console.log(error);
-        next(error);
+      next(error);
     }
-
-    
   }
-  removeCategory(req, res, next) {}
+  async getAllCategory(req, res, next) {
+    try {
+      const category = await categoryModel.aggregate([
+        {
+          $lookup: {
+            from: "categories",
+            localField: "_id",
+            foreignField: "parent",
+            //parent is same with id
+            as: "children",
+          },
+        },
+        {
+          $project: {
+            __v: 0,
+            "children.__v": 0,
+            "children.parent": 0,
+          },
+        },
+      ]);
+      return res.status(200).json({
+        data: {
+          statusCode: 200,
+          category,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async removeCategory(req, res, next) {
+    try {
+      const { id } = req.params;
+      const category = await this.checkExistCategory(id);
+      const deleteCategory = await categoryModel.deleteOne({ _id: category._id});
+      if (deleteCategory.deletedCount == 0)
+        throw createErrors.InternalServerError("This product was not removed");
+      return res.status(200).json({
+        data: {
+          statusCode: 200,
+          message: "the catagory was deleted successfully",
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+  async getAllParents(req, res, next) {
+    try {
+      const parents = await categoryModel.find({ parent: undefined });
+      return res.status(200).json({
+        data: {
+          statusCode: 200,
+          parents,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async getChildOfParents(req, res, next) {
+    try {
+      const { parent } = req.params;
+      const children = await categoryModel.find(
+        { parent },
+        { __v: 0, parent: 0 }
+      );
+      return res.status(200).json({
+        data: {
+          statusCode: 200,
+          children,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async checkExistCategory(id) {
+    const category = await categoryModel.findById(id);
+    console.log(category);
+    if (!category) throw createErrors.NotFound("the category does not exist");
+    return category
+  }
   editCategory(req, res, next) {}
-  getAllCategory(req, res, next) {}
   getCategoryByID(req, res, next) {}
-  getAllParents(req, res, next) {}
-  getChildOfParents(req, res, next) {}
 }
 
 module.exports = {
--- app/router/admin/category.js
+++ app/router/admin/category.js
@@ -7,7 +7,7 @@ const {categoryController} = require("../../http/controller/admin/category.contr
  * @swagger
  *  /admin/category/add:
  *      post:
- *          tags : [admin-Panel]
+ *          tags : [adminPanel]
  *          summary : create new category title
  *          parameters:
  *              -     in: formData
@@ -26,7 +26,69 @@ const {categoryController} = require("../../http/controller/admin/category.contr
  */
 
 router.post("/add" , categoryController.addCategory);
+/**
+ * @swagger
+ *  /admin/category/parents:
+ *      get:
+ *         tags : [adminPanel]
+ *         summary : get all parents of categories
+ *         responses:
+ *              200:
+ *                 description : successful
+ *              404:
+ *                 description: not successful
+ */
+router.get("/parents" , categoryController.getAllParents);
+/**
+ * @swagger
+ *  /admin/category/children/{parent}:
+ *      get:
+ *         tags : [adminPanel]
+ *         summary : get all child of parents category
+ *         parameters:
+ *              -    in: path
+ *                   type: string
+ *                   required : true
+ *                   name : parent
+ *         responses:
+ *              200:
+ *                 description : successful
+ *              404:
+ *                 description: not successful
+ */
+router.get("/children/:parent" , categoryController.getChildOfParents);
+/**
+ * @swagger
+ *  /admin/category/all:
+ *      get:
+ *         tags : [adminPanel]
+ *         summary : get all categories
+ *         responses:
+ *              200:
+ *                 description : successful
+ *              404:
+ *                 description: not successful
+ */
+router.get("/all" , categoryController.getAllCategory);
 
+/**
+ * @swagger
+ *  /admin/category/remove/{id}:
+ *      delete:
+ *         tags : [adminPanel]
+ *         summary : delete category By id
+ *         parameters:
+ *              -    in: path
+ *                   type: string
+ *                   required : true
+ *                   name : id
+ *         responses:
+ *              200:
+ *                 description : successful
+ *              404:
+ *                 description: not successful
+ */
+router.delete("/remove/:id" , categoryController.removeCategory);
 module.exports = {
   categoryRoutes: router,
 };
