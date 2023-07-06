d168bb2e13179c43a0fa178b376a145f722d7e1c
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-07-06 15:28:03 +0330

    Coding swagger part RBAC

---
+++ app/router/admin/swagger/premission.swagger.js
@@ -0,0 +1,110 @@
+/**
+ * @swagger
+ *  /admin/premission/add:
+ *      post:
+ *          tags: [RBAC(adminPanel)]
+ *          summary: create new premission
+ *          consumes:
+ *              -   multipart/form-data
+ *          parameters:
+ *              -   in: header
+ *                  name: access-token
+ *                  type: string
+ *                  value: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZSI6IjA5MzkyMzI3NDkzIiwiaWF0IjoxNjg4NDU4MzQ3LCJleHAiOjE2ODkzMjIzNDd9.JStuGyhHdh3d9VBYsZFLZjFu2Z8ar_A_Au3hyvgwqOw
+ *              -   in: formData
+ *                  name: title
+ *                  required : true
+ *                  type: string
+ *              -   in: formData
+ *                  name: premissions
+ *                  description: blog / course / product
+ *                  type: string
+ *                  enum: 
+ *                      -   blog
+ *                      -   course
+ *                      -   product
+ *          responses:
+ *               201:
+ *                  description: created
+ *               400:
+ *                  description: failed
+ */
+
+/**
+ * @swagger
+ *  /admin/premission/update/{id}:
+ *      patch:
+ *          tags: [RBAC(adminPanel)]
+ *          summary: edit the premission
+ *          consumes:
+ *              -   multipart/form-data
+ *          parameters:
+ *              -   in: header
+ *                  name: access-token
+ *                  type: string
+ *                  value: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZSI6IjA5MzkyMzI3NDkzIiwiaWF0IjoxNjg4NDU4MzQ3LCJleHAiOjE2ODkzMjIzNDd9.JStuGyhHdh3d9VBYsZFLZjFu2Z8ar_A_Au3hyvgwqOw
+ *              -   in: path
+ *                  name: id
+ *                  type: string
+ *                  required: true
+ *              -   in: formData
+ *                  name: title
+ *                  type: string
+ *              -   in: formData
+ *                  name: premissions
+ *                  description: blog / course / product
+ *                  type: string
+ *                  enum: 
+ *                      -   blog
+ *                      -   course
+ *                      -   product
+ *          responses:
+ *               200:
+ *                  description: created
+ *               400:
+ *                  description: failed
+ */
+
+/**
+ * @swagger
+ *  /admin/premission/remove/{id}:
+ *      delete:
+ *          tags: [RBAC(adminPanel)]
+ *          summary: edit the premission
+ *          consumes:
+ *              -   multipart/form-data
+ *          parameters:
+ *              -   in: header
+ *                  name: access-token
+ *                  type: string
+ *                  value: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZSI6IjA5MzkyMzI3NDkzIiwiaWF0IjoxNjg4NDU4MzQ3LCJleHAiOjE2ODkzMjIzNDd9.JStuGyhHdh3d9VBYsZFLZjFu2Z8ar_A_Au3hyvgwqOw
+ *              -   in: path
+ *                  name: id
+ *                  type: string
+ *                  required: true
+ *          responses:
+ *               200:
+ *                  description: created
+ *               400:
+ *                  description: failed
+ */
+
+/**
+ * @swagger
+ *  /admin/premission/list:
+ *      get:
+ *          tags: [RBAC(adminPanel)]
+ *          summary: get the premissions 
+ *          consumes:
+ *              -   multipart/form-data
+ *          parameters:
+ *              -   in: header
+ *                  name: access-token
+ *                  type: string
+ *                  value: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZSI6IjA5MzkyMzI3NDkzIiwiaWF0IjoxNjg4NDU4MzQ3LCJleHAiOjE2ODkzMjIzNDd9.JStuGyhHdh3d9VBYsZFLZjFu2Z8ar_A_Au3hyvgwqOw
+ *          responses:
+ *               200:
+ *                  description: created
+ *               400:
+ *                  description: failed
+ */
