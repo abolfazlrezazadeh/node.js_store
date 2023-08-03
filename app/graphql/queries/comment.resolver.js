3489f0f9bf00b8a969ac6f212f248b81a258db6f
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-08-03 14:56:38 +0330

    find a comment

--- app/graphql/queries/comment.resolver.js
+++ app/graphql/queries/comment.resolver.js
@@ -1,13 +1,13 @@
 const { GraphQLString } = require("graphql");
 const { commentType } = require("../typeDefs/comment.type");
 const { blogModel } = require("../../model/blog");
-const {StatusCodes: httpStatus} = require("http-status-codes");
+const { StatusCodes: httpStatus } = require("http-status-codes");
 const createHttpError = require("http-errors");
 const {
   vrefiyAccessTokenInGraphQL,
 } = require("../../http/middleware/verifyAccesssToken");
 const { responseType } = require("../typeDefs/public.type");
-
+const { copyObject } = require("../../utils/function");
 
 const createCommentForBlog = {
   type: responseType,
@@ -22,8 +22,10 @@ const createCommentForBlog = {
     const user = await vrefiyAccessTokenInGraphQL(req);
     const { comment, blogId, parent } = args;
     await checkExistBlog(blogId);
+    // check exist parent of comment
+    const commentDocument = await getComment(blogModel, parent);
     //push data
-    blogModel.updateOne(
+    await blogModel.updateOne(
       { _id: blogId },
       {
         $push: {
@@ -38,17 +40,24 @@ const createCommentForBlog = {
       }
     );
     return {
-        statusCode : httpStatus.CREATED,
-        data : {
-            message : "›omment was successfully registered and will be placed on the website after confirmation"
-        }
-    }
+      statusCode: httpStatus.CREATED,
+      data: {
+        message:
+          "Comment was successfully registered and will be placed on the website after confirmation",
+      },
+    };
   },
 };
 async function checkExistBlog(id) {
-  const blog = await blogModel.findOne(id);
+  const blog = await blogModel.findOne({ _id: id });
   if (!blog) throw createHttpError.NotFound("Blog not found");
   return blog;
+}
+async function getComment(model, id) {
+  const findedComment = await model.findOne({ "comments._id": id }, { "comments.$": 1 });
+  const comment = copyObject(findedComment);
+  if (!comment?.comments?.[0]) throw createHttpError.NotFound("comment not found");
+  return comment?.comments?.[0];
 }
 
 module.exports = {
