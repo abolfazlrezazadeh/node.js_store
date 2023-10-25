22449dc3a97695fd2cf3b03ec6b3da653db166e8
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-10-25 11:52:17 +0330

    create and get name spacesin namespacce controller

--- app/http/controller/support/namespace.controller.js
+++ app/http/controller/support/namespace.controller.js
@@ -1,6 +1,37 @@
+const { conversationModel } = require("../../../model/conversation");
 const controller = require("../controller");
+const createError = require("http-errors");
+const { StatusCodes: httpStatus } = require("http-status-codes");
 
-class namespacecontroller extends controller {}
+class namespacecontroller extends controller {
+  async addNamespace(req, res, next) {
+    try {
+      const { title, endpoints } = req.body;
+      const namespace = conversationModel.create({ title, endpoints });
+      return res.status(httpStatus.OK).json({
+        statusCode : httpStatus.OK,
+        data : {
+            message : 'Conversation space successfully created'
+        }
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async getListOfNamspaces(req, res, next) {
+    try {
+      const namespace = conversationModel.find({},{rooms : 0})
+      return res.status(httpStatus.CREATED).json({
+        statusCode : httpStatus.CREATED,
+        data : {
+            namespace
+        }
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
 
 module.exports = {
   namespaceController: new namespacecontroller(),
