ba8410f73dfb865efcaf956f80e084bc7433b12d
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-11-13 12:45:59 +0330

    adding check access middleware for support route

--- app/http/middleware/auth.js
+++ app/http/middleware/auth.js
@@ -1,23 +1,38 @@
-const { userModel } = require("../../model/users")
+const { userModel } = require("../../model/users");
 
-async function checkLogin(req,res,next){
-    try {
-        const token = req.signedCookies["authorization"]
-        if(token){
-            const user = userModel.findOne({token})
-            if(user){
-                req.user = user
-                return next();
-            }
-        }
-        return res.render('login.ejs',{
-            error : "please login to your account "
-        })
-    } catch (error) {
-        next(error)
+async function checkLogin(req, res, next) {
+  try {
+    const token = req.signedCookies["authorization"];
+    if (token) {
+      const user = userModel.findOne({ token });
+      if (user) {
+        req.user = user;
+        return next();
+      }
     }
+    return res.render("login.ejs", {
+      error: "please login to your account ",
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+async function checkAccess(req, res, next) {
+  try {
+    const token = req.signedCookies["authorization"];
+    if (token) {
+      const user = userModel.findOne({ token });
+      if (user) {
+        return res.redirect("/support");
+      }
+    }
+    return next();
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
-    checkLogin
-}
+  checkLogin,
+  checkAccess
+};
--- app/router/support/support.router.js
+++ app/router/support/support.router.js
@@ -1,15 +1,15 @@
 const {
   supportController,
 } = require("../../http/controller/support/support.controller");
-const { checkLogin } = require("../../http/middleware/auth");
+const { checkLogin, checkAccess } = require("../../http/middleware/auth");
 const { namespaceAdminRoute } = require("./namespace.router");
 const { roomAdminRoute } = require("./room.router");
 
 const router = require("express").Router();
 router.use("/namespace", namespaceAdminRoute);
 router.use("/room", roomAdminRoute);
-router.post("/login", supportController.postLoginForm);
-router.get("/login", supportController.getLoginForm);
+router.post("/login", checkAccess, supportController.postLoginForm);
+router.get("/login", checkAccess, supportController.getLoginForm);
 router.get("/", checkLogin, supportController.renderFile);
 // router.use("/namespace", namespaceAdminRoute);
 module.exports = {
